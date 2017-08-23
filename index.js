054c26751a351408c4377fe1aed08aacd793406e
Nguyen Phuc Thien An <npta97@gmail.com>
2017-08-23 13:22:37 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+const config = require('./config');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config.mongoURI);
+
+app.use(bodyParser.json());
+
+app.use('/', routes);
+
+app.listen(config.port, () => {
+  console.log(`Server listening on PORT ${config.port}`);
+});
