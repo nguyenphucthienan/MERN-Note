1511efb9f6258caa563802b26ea2fd7cbbf8fa72
Nguyen Phuc Thien An <npta97@gmail.com>
2017-08-26 15:34:50 +0700

    Add Authentication with Google OAuth 2.0

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const router = express.Router();
 
+const authController = require('../controllers/authController');
+const requireLogin = require('../middlewares/requireLogin');
+
 router.get('/', (req, res) => {
   res.send({ hi: 'there' });
 });
+
+router.get('/api/login/google', authController.googleLogin);
+
+router.get('/api/login/google/callback',
+  authController.googleLoginCallback,
+  authController.googleCallback
+);
+
+router.get('/api/logout', authController.logout);
+
+router.get('/api/me',
+  requireLogin,
+  authController.currentUser
+);
 
 module.exports = router;
